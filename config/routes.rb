c1bdbfc010ec561b7b9989500ecb8345f8094701
Tsutomu Kurafuji <bull.bull.bull91@gmail.com>
2017-11-26 09:56:21 +0000

    pictureカラム削除前

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :users do
     member do
-      get :following, :followers
+      #get :following, :followers
     end
   end
   resources :account_activations, only: [:edit]
