43a62c51e58fbc7c0dfeb54ab3c4691ac8ddcbae
Tsutomu Kurafuji <bull.bull.bull91@gmail.com>
2017-11-26 04:56:59 +0000

    競合削除_20171126

--- app/controllers/application_controller_BACKUP_2674.rb
+++ app/controllers/application_controller_BACKUP_2674.rb
@@ -5,18 +5,10 @@ class ApplicationController < ActionController::Base
 
     # ユーザーのログインを確認する
     def logged_in_user
-<<<<<<< HEAD
       unless logged_in?
         store_location
         flash[:danger] = "ログインしてください"
         redirect_to login_url
       end
-=======
-      #unless logged_in?
-       # store_location
-        #flash[:danger] = "Please log in."
-        #redirect_to login_url
-      
->>>>>>> origin/master
     end
 end
